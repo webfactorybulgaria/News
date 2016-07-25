7b095d6670c6b7e5b8ab427e3a6320e577085160
Nasko <nasko@webfactory.bg>
2016-07-25 12:40:17 +0300

    pagination

--- src/resources/views/admin/index.blade.php
+++ src/resources/views/admin/index.blade.php
@@ -9,7 +9,7 @@
     @include('core::admin._button-create', ['module' => 'news'])
 
     <h1>
-        <span>@{{ models.length }} @choice('news::global.news', 2)</span>
+        <span>@{{ totalModels }} @choice('news::global.news', 2)</span>
     </h1>
 
     <div class="btn-toolbar">
@@ -17,19 +17,37 @@
     </div>
 
     <div class="table-responsive">
-
-        <table st-persist="newsTable" st-table="displayedModels" st-safe-src="models" st-order st-filter class="table table-condensed table-main">
+        <table st-persist="newsTable" st-table="displayedModels" st-order st-sort-default="date" st-sort-default-reverse="true" st-pipe="callServer" st-filter class="table table-condensed table-main">
             <thead>
+                <tr>
+                    <td colspan="6" st-items-by-page="itemsByPage" st-pagination="" st-template="/views/partials/pagination.custom.html"></td>
+                </tr>
                 <tr>
                     <th class="delete"></th>
                     <th class="edit"></th>
                     <th st-sort="status" class="status st-sort">Status</th>
                     <th st-sort="image" class="image st-sort">Image</th>
-                    <th st-sort="date" st-sort-default="reverse" class="date st-sort">Date</th>
+                    <th st-sort="date" class="date st-sort">Date</th>
                     <th st-sort="title" class="title st-sort">Title</th>
                 </tr>
                 <tr>
-                    <td colspan="5"></td>
+                    <td colspan="2"></td>
+                    <td>
+                        <select class="form-control" st-input-event="change keydown" st-search="status.boolean">
+                            <option value=""></option>
+                            <option value="true">Active</option>
+                            <option value="false">Not Active</option>
+                        </select>
+                    </td>
+                    <td colspan="1"></td>
+                    <td>
+                        <datepicker date-format="yyyy-MM-dd" class="filter-date">
+                            <input type="text" st-search="date.date.filter_from" class="form-control input-sm" placeholder="From date…">
+                        </datepicker>
+                        <datepicker date-format="yyyy-MM-dd" class="filter-date">
+                            <input type="text" st-search="date.date.filter_to" class="form-control input-sm" placeholder="To date…">
+                        </datepicker>
+                    </td>
                     <td>
                         <input st-search="title" class="form-control input-sm" placeholder="@lang('global.Search')…" type="text">
                     </td>
@@ -52,7 +70,10 @@
             </tbody>
             <tfoot>
                 <tr>
-                    <td colspan="6" typi-pagination></td>
+                    <td colspan="5" st-items-by-page="itemsByPage" st-pagination="" st-template="/views/partials/pagination.custom.html"></td>
+                    <td>
+                        <div ng-include="'/views/partials/pagination.itemsPerPage.html'"></div>
+                    </td>
                 </tr>
             </tfoot>
         </table>
